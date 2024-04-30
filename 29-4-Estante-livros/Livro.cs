86a97fbc67a336923b675e353ce457bf90fe2012
JulianaMistron <jumistron@yahoo.com.br>
2024-04-30 09:10:01 -0300

    Add project files.

---
+++ 29-4-Estante-livros/Livro.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Livro
+{
+    public string titulo;
+    public string autor;
+    public DateOnly lancamento;
+    public string editora;
+    public int edicao;
+    public int isbn;
+    public int qtpag;
+
+    public Livro()
+    {
+    }
+
+    public void DefinirTitulo(string t)
+    {
+        this.titulo = t;
+    }
+    public void DefinirAutor(string a)
+    {
+        this.autor = a;
+    }
+    public void DefinirDataLancamento(DateOnly l)
+    {
+        this.lancamento = l;
+    }
+    public void DefinirEditora(string e)
+    {
+        this.editora = e;
+    }
+    public void DefinirEdicao(int ed)
+    {
+        this.edicao = ed;
+    }
+    public void DefinirIsbn(int isb)
+    {
+        this.isbn = isb;
+    }
+    public void DefinirQtPag(int pag)
+    {
+        this.qtpag = pag;
+    }
+    public void ImprimirLivro(Livro[] livros, int posicao)
+    {
+        Console.WriteLine("\nLivro da posição: " + posicao);
+        Console.WriteLine("\nLivro da posição: " + livros[posicao].titulo);
+        Console.WriteLine("Título: " + livros[posicao].autor);
+        Console.WriteLine("Autor: " + livros[posicao].lancamento);
+        Console.WriteLine("Data de lançamento: " + livros[posicao].editora);
+        Console.WriteLine("Editora: " + livros[posicao].edicao);
+        Console.WriteLine("ISBN: " + livros[posicao].isbn);
+        Console.WriteLine("Quantidade de páginas: " + livros[posicao].qtpag);
+
+    }
+
+    public void ImprimirLivros(Livro[] livros)
+    {
+        for (int i = 0; i < livros.Length; i++)
+        {
+            Console.WriteLine("\nLivro da posição: " + (i + 1));
+            Console.WriteLine("Título: " + livros[i].titulo);
+            Console.WriteLine("Autor: " + livros[i].autor);
+            Console.WriteLine("Data de lançamento: " + livros[i].lancamento);
+            Console.WriteLine("Editora: " + livros[i].editora);
+            Console.WriteLine("Edição: " + livros[i].edicao);
+            Console.WriteLine("ISBN: " + livros[i].isbn);
+            Console.WriteLine("Quantidade de páginas: " + livros[i].qtpag);
+            Console.WriteLine("**********************************");
+        }
+    }
+}
---
+++ 29-4-Estante-livros/Program.cs
@@ -0,0 +1,52 @@
+﻿//criar estante de livros, onde a estante é um vetor de 10 livros fixo e depois imprimir a estante inteira ou a
+//posição q eu escoher, informações relevantes a um livro; titulo, autor/es, data lancamento, editora, edição,
+//isbn, qtde pg,...inserção sequencial
+int size = 10;
+Livro[] livros = new Livro[size];
+
+for (int i = 0; i < size; i++)
+{
+    Livro l1 = new Livro();
+    int numeroDoLivro = i + 1;
+
+    Console.WriteLine($"Livro {numeroDoLivro}: ");
+    l1.DefinirTitulo(Console.ReadLine());
+
+    Console.WriteLine("Digite o autor/es: ");
+    l1.DefinirAutor(Console.ReadLine());
+
+    Console.WriteLine("Digite a data de lançamento: ");
+    l1.DefinirDataLancamento(DateOnly.Parse(Console.ReadLine()));
+
+    Console.WriteLine("Digite a editora: ");
+    l1.DefinirEditora(Console.ReadLine());
+
+    Console.WriteLine("Digite o número da edição: ");
+    l1.DefinirEdicao(int.Parse(Console.ReadLine()));
+
+    Console.WriteLine("Digite o ISBN: ");
+    l1.DefinirIsbn(int.Parse(Console.ReadLine()));
+
+    Console.WriteLine("Digite a quantidade de páginas: ");
+    l1.DefinirQtPag(int.Parse(Console.ReadLine()));
+
+    livros[i] = l1;
+}
+
+Console.ReadKey();
+
+Console.WriteLine("Informe a opção desejada: ");
+Console.WriteLine("1 - Imprimir todos os livros: ");
+Console.WriteLine("2 - Escolher a posição do livro na estante: ");
+
+int opcao = int.Parse(Console.ReadLine());
+Livro livro = new Livro();
+if (opcao == 1)
+
+    livro.ImprimirLivros(livros);
+else
+{
+    Console.WriteLine("Digite a posição do livro na estante"); 
+    int posicao = int.Parse(Console.ReadLine());
+    livro.ImprimirLivro(livros, posicao);
+}
